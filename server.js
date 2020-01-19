af13d94e8c79a22c57e404afd9de46b3d1ee2637
Asli Ari <asliari2012@gmail.com>
2020-01-19 19:56:46 +0100

    Setup backend

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+mongoose.Promise = global.Promise;
+const port = process.env.PORT || 3030;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/contact-list');
+
+var app = express();
+
+app.use('/', require('./routes/api'));
+
+
+// Handle production and SPA
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static(__dirname + '/public/'));
+	app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
+app.listen(port, () => {
+	console.log('Server is running on:', port);
+})
+
